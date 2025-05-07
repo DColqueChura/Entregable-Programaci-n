998c0460adaec0653ab2ef9c9b3a56700fb0e73e
DanikaCC <90420154+DColqueChura@users.noreply.github.com>
2025-05-07 20:26:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "funciones.h"
+#define CANT_ARCHIVOS 4
+
+
 
 int main(){
     char *categorias[] = {
         "planetas.txt", 
-        //"estrellas.txt", 
-        //"cometas.txt", 
-        //"satelites.txt"
+        "estrellas.txt", 
+        "cometas.txt", 
+        "satelites.txt"
     };
     
     //Declara un array de 4 punteros (desde archivos[0] a archivos[3]) a FILE
-    FILE *archivo[4];
+    FILE *archivo[CANT_ARCHIVOS];
 
     //Bucle para abrir cada archivo
-    for(int i = 0; i<4; i++){
+    for(int i = 0; i<CANT_ARCHIVOS; i++){
         archivo[i] = fopen(categorias[i], "r");
         
-        //Comprueba que se haya abierto el archivo. Si no puede abrirlo, el archivo retorna NULL
+        //Manejo de errores
         if(!archivo[i]){
             //Imprime en stderr un mensaje de error.
             perror(categorias[i]);   //Ej. <nombre del archivo> “No such file or directory”
@@ -27,11 +31,31 @@ int main(){
         }
     }
     
-    /*CODIGO*/
+    // 2) Crear un array de punteros a Lista_T y reservar memoria para cada lista
+    Lista_T *catego[CANT_ARCHIVOS];
+    for (int i = 0; i < CANT_ARCHIVOS; i++) {
+        catego[i] = malloc(sizeof *catego[i]);
+        if (!catego[i]) {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+        CrearLista(catego[i]);    // inicializa la lista vacía
+    }
+
+    int opcion = 0;
+
+    //Llena las 1 listas por cada archivo. Los archivos son 4.
+    function(archivo, catego);
+
+    //Imprime la lista, dada una elección de la categoría = opcion
+    RecorrerLista(catego[opcion]);
+
+    
     
     //Bucle para cerrar los archivos
-    for(int i = 0; i<4; i++)
+    for(int i = 0; i<CANT_ARCHIVOS; i++){
+        free(catego[i]);
         fclose(archivo[i]);
-    
+    }
     return 0;
 }
