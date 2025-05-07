4f1517027d859d3d0012820c340dad5da1071d2c
DanikaCC <90420154+DColqueChura@users.noreply.github.com>
2025-05-06 22:17:03 -0300

    Tarea 1. Abre los 4 archivos y los cierra al final del programa.

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include "funciones.h"
 
 int main(){
-    char palabra[]="HOLA!";
-    printf("%s\n", palabra);
-    cadena_a_minusculas(palabra);
-    printf("%s\n", palabra);
+    char *categorias[] = {
+        "planetas.txt", 
+        //"estrellas.txt", 
+        //"cometas.txt", 
+        //"satelites.txt"
+    };
+    
+    //Declara un array de 4 punteros (desde archivos[0] a archivos[3]) a FILE
+    FILE *archivo[4];
 
+    //Bucle para abrir cada archivo
+    for(int i = 0; i<4; i++){
+        archivo[i] = fopen(categorias[i], "r");
+        
+        //Manejo de errores
+        if(!archivo[i]){
+            //Imprime en stderr un mensaje de error.
+            perror(categorias[i]);   //Ej. <nombre del archivo> “No such file or directory”
+            
+            //Termina inmediatamente el programa con un código de error.
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    /*CODIGO*/
+    
+    //Bucle para cerrar los archivos
+    for(int i = 0; i<4; i++)
+        fclose(archivo[i]);
+    
     return 0;
 }
